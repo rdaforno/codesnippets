2f9aeab2cfdf0107cf7f33bceb4c75cb0ee4539c
rdaforno <keepcoding@gmx.net>
2020-02-27 20:33:47 +0100

    Update file_renamer.py
    renaming script now accepts multiple files (drag & drop)

--- python/file_renamer.py
+++ python/file_renamer.py
@@ -10,22 +10,24 @@ replacebywhitespace = [ ".", "_", "(", ")" ]
 
 logfile = "rename.log"
 
-if len(sys.argv) < 2 or not os.path.isfile(sys.argv[1]):
-    sys.exit()
-filename = sys.argv[1]
-f = open(logfile, "a", encoding="utf-8")
-try:
-    [newfilename, fileext] = os.path.splitext(os.path.basename(filename))
-    for str in stringstodelete:
-        newfilename = newfilename.replace(str, "")
-    for str in replacebywhitespace:
-        newfilename = newfilename.replace(str, " ")
-    # replace non-ascii characters
-    newfilename = "".join(c for c in newfilename if ord(c) < 128)
-    # merge multiple whitespaces
-    newfilename = ' '.join(newfilename.split())
-    f.write("%s  file '%s' renamed to '%s%s'\n" % (datetime.now().strftime("%Y-%m-%d %H:%M:%S"), os.path.basename(filename), newfilename, fileext.lower()))
-    os.rename(filename, "%s/%s%s" % (os.path.dirname(filename), newfilename, fileext.lower()))
-except Exception as e:
-    f.write("%s  an error occurred: %s\n" % (datetime.now().strftime("%Y-%m-%d %H:%M:%S"), e))
+for i in range(1, len(sys.argv)):
+    if not os.path.isfile(sys.argv[i]):
+        print("%s is not a file" % sys.argv[i])
+        continue
+    filename = sys.argv[i]
+    try:
+        [newfilename, fileext] = os.path.splitext(os.path.basename(filename))
+        for str in stringstodelete:
+            newfilename = newfilename.replace(str, "")
+        for str in replacebywhitespace:
+            newfilename = newfilename.replace(str, " ")
+        # replace non-ascii characters
+        newfilename = "".join(c for c in newfilename if ord(c) < 128)
+        # merge multiple whitespaces
+        newfilename = ' '.join(newfilename.split())
+        f.write("%s  file '%s' renamed to '%s%s'\n" % (datetime.now().strftime("%Y-%m-%d %H:%M:%S"), os.path.basename(filename), newfilename, fileext.lower()))
+        os.rename(filename, "%s/%s%s" % (os.path.dirname(filename), newfilename, fileext.lower()))
+    except Exception as e:
+        f.write("%s  an error occurred: %s\n" % (datetime.now().strftime("%Y-%m-%d %H:%M:%S"), e))
+
 f.close()
