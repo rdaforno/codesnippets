f165f68174edd876d71d64116c91a3ebfb3dcc03
rdaforno <rdaforno@ethz.ch>
2023-03-07 20:03:44 +0100

    minor update

--- python/file_renamer.py
+++ python/file_renamer.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 
-import sys, os
+import sys, os, re
 from datetime import datetime
 
 # strings to remove
 stringstodelete = [ "unwanted_string1", "unwanted_string2" ]
 # chars to replace by a whitespace
-replacebywhitespace = [ ".", "_", "(", ")" ]
+replacebywhitespace = [ ".", "_", "(", ")", "[", "]" ]
 
 logfile = "rename.log"
+f = open(logfile, "a", encoding="utf-8")
 
 for i in range(1, len(sys.argv)):
     if not os.path.isfile(sys.argv[i]):
@@ -17,12 +18,16 @@ for i in range(1, len(sys.argv)):
     filename = sys.argv[i]
     try:
         [newfilename, fileext] = os.path.splitext(os.path.basename(filename))
+        # remove unwanted words
         for str in stringstodelete:
             newfilename = newfilename.replace(str, "")
+        # replace special characters by whitespaces
         for str in replacebywhitespace:
             newfilename = newfilename.replace(str, " ")
-        # replace non-ascii characters
-        newfilename = "".join(c for c in newfilename if ord(c) < 128)
+        # replace non-ascii characters and leading/trailing whitespaces
+        newfilename = "".join(c for c in newfilename if ord(c) < 128).strip()
+        # put year in brackets (if at the end)
+        newfilename = re.sub(r'([0-9]{4})$', r'(\1)', newfilename)
         # merge multiple whitespaces
         newfilename = ' '.join(newfilename.split())
         f.write("%s  file '%s' renamed to '%s%s'\n" % (datetime.now().strftime("%Y-%m-%d %H:%M:%S"), os.path.basename(filename), newfilename, fileext.lower()))
